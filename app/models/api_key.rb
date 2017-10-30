f4104707ba002b9d65f2145f360d3c9f335385f3
Adam Dill <altrux@gmail.com>
2017-10-29 23:34:53 -0600

    [API] random notes

--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -1,4 +1,6 @@
 class ApiKey < ApplicationRecord
+  # API Keys belong to an Applications and are used to interface with the API
+  # We use the Rails 5 has_secure_token methods to generate the token
   belongs_to :application
   has_secure_token :auth_token
   validates :application, presence: true
